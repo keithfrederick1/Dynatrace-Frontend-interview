ee4fb28183e58e91a2edb3efb4f4ad0fbcb7ebef
keithfrederick1 <kfrederick1004@gmail.com>
2022-06-12 23:40:34 -0700

    input fields functionality added

--- client/src/components/InputFields.tsx
+++ client/src/components/InputFields.tsx
@@ -1,26 +1,28 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import axios from "axios";
 import * as React from "react";
+import { useContext, useState } from 'react';
+import Context from '../Context';
 
-const handleSubmit = () => {
-  //post
-  axios
-    .post("", {})
-    .then(({ data }) => {
-      //add response to data array
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-};
-
-const handleCancel = () => {
-  //redirect to sales lead list
-};
+//complete fix context
 
 const ValidationTextFields = () => {
+  const { handleLeadSubmit, setShowLeads } = useContext(Context);
+  const [leadName, setLeadName] = useState<string>('');
+  const [salesRep, setSalesRep] = useState<string>('');
+  const [client, setClient] = useState<string>('');
+  const [value, setValue] = useState<string>('');
+  const [date, setDate] = useState<string>('');
+
+  const newLead = {
+    name: leadName,
+    value: value,
+    date: date,
+    ownerName: salesRep,
+    clientName: client
+  };
+
   return (
     <div>
       <h2>Add sales lead</h2>
@@ -34,23 +36,24 @@ const ValidationTextFields = () => {
         autoComplete="off"
       >
         <div>
-          <TextField id="outlined-basic" label="Lead name" variant="outlined" />
+          <TextField id="outlined-basic" label="Lead name" variant="outlined" onChange={(e) => setLeadName(e.target.value)}/>
           <br />
           <TextField
             id="outlined-basic"
             label="Sales representative "
             variant="outlined"
+            onChange={(e) => setSalesRep(e.target.value)}
           />
           <br />
-          <TextField id="outlined-basic" label="Client" variant="outlined" />
+          <TextField id="outlined-basic" label="Client" variant="outlined" onChange={(e) => setClient(e.target.value)}/>
           <br />
-          <TextField id="outlined-basic" label="Value" variant="outlined" />
+          <TextField id="outlined-basic" label="Value" variant="outlined" onChange={(e) => setValue(e.target.value)}/>
           <br />
-          <TextField id="outlined-basic" label="Date" variant="outlined" />
+          <TextField id="outlined-basic" label="Date" variant="outlined" onChange={(e) => setDate(e.target.value)}/>
         </div>
       </Box>
-      <Button variant="outlined">Cancel</Button>
-      <Button variant="contained">Save</Button>
+      <Button variant="outlined" onClick={() => setShowLeads(false)}>Cancel</Button>
+      <Button variant="contained" onClick={() => handleLeadSubmit(newLead)}>Save</Button>
     </div>
   );
 }
